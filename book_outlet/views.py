7617493cbf21ef43d7189d60f5ecbf3273dd8033
devinaacs <dde.vina@yahoo.co.id>
2024-07-01 23:27:49 +0700

    using the slug & updating field options

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Book
 from django.http import Http404
 # Create your views here.
@@ -10,11 +10,20 @@ def index(request):
         "books": books
     })
 
-def book_detail(request, id):
-    try:
-        book = Book.objects.get(pk=id)
-    except:
-        raise Http404
+# def book_detail(request, id):
+#     try:
+#         book = Book.objects.get(pk=id)
+#     except:
+#         raise Http404
+#     return render(request, "book_outlet/book_detail.html", {
+#         "title": book.title,
+#         "author": book.author,
+#         "rating": book.rating,
+#         "is_bestselling": book.is_bestselling
+#     })
+
+def book_detail(request, slug):
+    book = get_list_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
